59c1e1070aec1fc5928104c29ff5418307587614
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-29 18:41:31 +1100

    do this otherwise config will be pulled from the wrong place

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+REPO="Abinash3366/elk-stack"
+
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add -
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
@@ -16,13 +18,13 @@ echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee 
 sudo apt-get update && sudo apt-get install packetbeat filebeat metricbeat -y
 
 # Setup filebeat
-curl -L "https://raw.githubusercontent.com/shaunakg/elk-stack/main/beat-configs/filebeat.yml" | sudo tee /etc/filebeat/filebeat.yml
+curl -L "https://raw.githubusercontent.com/$REPO/main/beat-configs/filebeat.yml" | sudo tee /etc/filebeat/filebeat.yml
 
 # Setup metricbeat
-curl -L "https://raw.githubusercontent.com/shaunakg/elk-stack/main/beat-configs/metricbeat.yml" | sudo tee /etc/metricbeat/metricbeat.yml
+curl -L "https://raw.githubusercontent.com/$REPO/main/beat-configs/metricbeat.yml" | sudo tee /etc/metricbeat/metricbeat.yml
 
 # Setup packetbeat
-curl -L "https://raw.githubusercontent.com/shaunakg/elk-stack/main/beat-configs/packetbeat.yml" | sudo tee /etc/packetbeat/packetbeat.yml
+curl -L "https://raw.githubusercontent.com/$REPO/main/beat-configs/packetbeat.yml" | sudo tee /etc/packetbeat/packetbeat.yml
 
 sudo service filebeat restart
 sudo service metricbeat restart
