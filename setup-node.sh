642439a2726981fc4796ac377c948c59f6831ba8
Shaunak G <gadkari.shaunak@gmail.com>
2021-10-30 14:43:18 +1100

    Update and rename setup.sh to setup-node.sh

--- setup-node.sh
+++ setup-node.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "========================================================="
+echo "Setting up edge node for logging and network monitoring."
+echo "========================================================="
+
+echo "[INFO] Installing Tailscale"
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.gpg | sudo apt-key add -
 curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/focal.list | sudo tee /etc/apt/sources.list.d/tailscale.list
 
 sudo apt-get update
 sudo apt-get install tailscale
-sudo tailscale up --authkey "$1"
+
+echo "[INFO] Tailscale is installed. It will be set up later."
+echo "[INFO] Installing beats"
 
 mkdir beats
 cd beats
@@ -24,6 +31,16 @@ curl -L "https://raw.githubusercontent.com/shaunakg/elk-stack/main/beat-configs/
 # Setup packetbeat
 curl -L "https://raw.githubusercontent.com/shaunakg/elk-stack/main/beat-configs/packetbeat.yml" | sudo tee /etc/packetbeat/packetbeat.yml
 
-sudo service filebeat restart
-sudo service metricbeat restart
-sudo service packetbeat restart
+echo "[INFO] Installation complete."
+
+echo "[INFO] Authorizing Tailscale before starting beats."
+echo "[INFO] If you didn't include an authkey as the first argument, you now need to authorize your Tailscale account manually."
+sudo tailscale up --authkey "$1"
+
+echo "[INFO] Starting all beats"
+
+sudo service filebeat restart && echo "[INFO] Filebeat started"
+sudo service metricbeat restart && echo "[INFO] Metricbeat started"
+sudo service packetbeat restart && echo "[INFO] Packetbeat started"
+
+echo "[INFO] Done. For next steps, visit https://github.com/shaunakg/elk-stack"
